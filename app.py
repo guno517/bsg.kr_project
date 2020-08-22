f0c0ea46817f5f2076da02db06a09e30baf820f3
guno517 <60571418+guno517@users.noreply.github.com>
2020-08-22 13:07:29 +0900

    user_info, user_mastery

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, request
 import os
 import requests
+from bs4 import BeautifulSoup
 from pprint import pprint as pp
 
 app = Flask(__name__)
 
 apikey = os.environ['LoL-API-KEY']
+
+
+#
+# url = 'https://www.op.gg/summoner/userName=' + Name
+# req = requests.get(url, headers=hdr)
+# html = req.text
+# soup = BeautifulSoup(html, 'html.parser')
 
 
 @app.route('/')
@@ -26,67 +34,103 @@ def data():
     url_SummonerName = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{}".format(sum_name)
     res_SummonerName = requests.get(url=url_SummonerName, headers=headers)
     account_Id = res_SummonerName.json()['accountId']  # account Id 가져오기
+    encryptedSummonerId = res_SummonerName.json()['id']  # encryptedId 가져오기
 
     url_Matches = "https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/{}".format(account_Id)
     res_Matches = requests.get(url=url_Matches, headers=headers)
     matches = res_Matches.json()['matches']
-    # url_MatchId = "https://kr.api.riotgames.com/lol/match/v4/matches{}".format(Matches)
-    # res_MatchId = requests.get(url=url_MatchId, headers=headers)
-    print(matches)
 
-    # url_MatchId = "https://kr.api.riotgames.com/lol/match/v4/matches{}".format(match_Id)
-    # res_MatchId = requests.get(url=url_MatchId, headers=headers)
-    # Teams = res_GameId.json()['']
+    url_league = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/{}".format(encryptedSummonerId)
+    res_league = requests.get(url=url_league, headers=headers)
+    user_infos = res_league.json()
+
+    print(user_infos)
+    print('-------------------------------')
+
+    url_Mastery = "https://kr.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/{}".format(
+        encryptedSummonerId)
+    res_Mastery = requests.get(url=url_Mastery, headers=headers)
+    user_masteries = res_Mastery.json()
+
+    print(user_masteries)
+    print('-------------------------------')
+
+    def get_champ_mastery(user_mastery):
+        resp_mastery = [
+            user_mastery.get('championId'),
+            user_mastery.get('championLevel'),
+            user_mastery.get('championPoints'),
+            user_mastery.get('championPointsUntilNextLevel'),
+        ]
+        return resp_mastery
+
+    results_champ_mastery = []
+    for user_mastery in user_masteries:
+        results_champ_mastery.append(get_champ_mastery(user_mastery))
+    length_champ_mastery = len(results_champ_mastery)
+
+    print(results_champ_mastery)
+    print('-------------------------------')
+
+    def get_user_info(user_info):
+        resp_user_info = [
+            user_info.get('queueType'),
+            user_info.get('tier'),
+            user_info.get('rank'),
+            user_info.get('wins'),
+            user_info.get('losses'),
+            user_info.get('leagueName'),
+            user_info.get('leaguePoints')
+        ]
+
+        return resp_user_info
+
+    results_user_info = []
+    for user_info in user_infos:
+        results_user_info.append(get_user_info(user_info))
+    length_user_info = len(results_user_info)
+
     Game_Ids = []
     Match_list = []
     for match in range(0, 20):
         Game_Ids.append(matches[match].get('gameId'))
         Match_list.append(matches[match])
 
-    #print(Match_list)
-    #print(Game_Ids)
+    url_MatchId = "https://kr.api.riotgames.com/lol/match/v4/matches{}".format(Game_Ids[0])
+    res_MatchId = requests.get(url=url_MatchId, headers=headers)
 
-    return render_template('search.html', sum_name=sum_name, Game_Ids=Game_Ids, Match_list=Match_list)
+    result_list = []
+    player_list = []
+    teams_list = []
 
+    for gameid in Game_Ids:
+        url_gameId = "https://kr.api.riotgames.com/lol/match/v4/matches/" + str(gameid)
+        res_gameId = requests.get(url_gameId, headers=headers)
+        data = res_gameId.json()
+        players = res_gameId.json()["participants"]
+        teams = res_gameId.json()['teams']
+        result = "패배"
+        if (data["teams"][0]["win"] == "Fail"):
+            for i in range(5, 10):
+                if (data["participantIdentities"][i]["player"]["summonerName"].replace(" ", "").lower() == sum_name):
+                    result = "승리"
+        if (data["teams"][0]["win"] == "Win"):
+            for i in range(0, 5):
+                if (data["participantIdentities"][i]["player"]["summonerName"].replace(" ", "").lower() == sum_name):
+                    result = "승리"
 
-# @app.route('/data')
-# def search():
-#     sum_name = request.args.get('name')
-#     url = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{}".format(sum_name)
-#     headers = {
-#         "Origin": "https://developer.riotgames.com",
-#         "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-#         "X-Riot-Token": apikey,
-#         "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
-#         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
-#     }
-#     res = requests.get(url=url, headers=headers)
-#     encrypted_id = res.json()['id']
-#     url_league = "https://kr.api.riotgames.com/lol/league/v4/positions/by-summoner/{}".format(encrypted_id)
-#     res_league = requests.get(url=url_league, headers=headers)
-#     league_dicts = res_league.json()
-#
-#     print(league_dicts)
-#
-#     print('-------------------------------')
-#     def get_league_info(league_dict):
-#         resp = [
-#             league_dict.get('queueType'),
-#             league_dict.get('tier'),
-#             league_dict.get('rank'),
-#             league_dict.get('wins'),
-#             league_dict.get('losses'),
-#             league_dict.get('leagueName'),
-#             league_dict.get('leaguePoints')
-#         ]
-#         return resp
-#
-#     results = []
-#     for league_dict in league_dicts:
-#         results.append(get_league_info(league_dict))
-#     length = len(results)
-#
-#     return render_template('search.html', sum_name=sum_name, results=results, length=length)
+        result_list.append(result)
+        player_list.append(players)
+        teams_list.append(teams)
+
+    print(teams_list)
+
+    # print(result_list) // 승리 패배만 뽑아줌
+
+    return render_template('search.html', sum_name=sum_name, Game_Ids=Game_Ids, Match_list=Match_list,
+                           player_list=player_list, teams_list=teams_list, results_user_info=results_user_info,
+                           results_champ_mastery=results_champ_mastery, length_champ_mastery=length_champ_mastery,
+                           length_user_info=length_user_info)
 
 
 if __name__ == '__main__':
